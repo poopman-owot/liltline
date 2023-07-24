b23fd7c3317fef2c3cd8ad935921a7c6840fda22
poopman <kylecypher@gmail.com>
2023-07-24 11:33:39 -0500

    Update app.js

--- app.js
+++ app.js
@@ -232,6 +232,7 @@ function handleKeyDown(event) {
   } else if (event.key === " ") {
     spacebarPressed = true;
   }
+console.log(event.key)
 }
 
 function handleKeyUp(event) {
@@ -256,7 +257,7 @@ function checkCollision() {
       // Collision detected and spacebar is pressed
       playerDistance += 1000; // Add 1000 points to the player's score
       spacebarPressed = false; // Reset the spacebar flag to prevent multiple score increments
-
+      console.log("1000 points!");
 blinkCanvasBackground(true);
             const wallIndex = yellowWalls.indexOf(wall);
       if (wallIndex !== -1) {
